12aa98bcf6d0a5e6d0c82db279480c70df96d209
DogeFox <lgfalconch@hotmail.com>
2023-04-19 20:20:53 -0500

    Hash Finalizado

--- microservices-backend/helpers/hash.js
+++ microservices-backend/helpers/hash.js
@@ -1,12 +1,12 @@
-const hash = (text) => {
+'use strict';
+const hash = async (text) => {
     const utf8 = new TextEncoder().encode(text)
-    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-        const hashArray = Array.from(new Uint8Array(hashBuffer))
-        const hashHex = hashArray
-            .map((bytes => bytes.toString(16).padStart(2,'0')))
-            .join('')
-        return hashHex
-    })
+    const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((bytes => bytes.toString(16).padStart(2, '0')))
+        .join('');
+    return hashHex;
 }
 
-module.exports = () => { hash }
+module.exports =  hash 
