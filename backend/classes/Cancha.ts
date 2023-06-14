88beb0d854459efbbb03b637cc6021718906a459
JorgeCostilla1 <20190542@aloe.ulima.edu.pe>
2023-06-13 22:03:10 -0500

    back cancha

--- backend/classes/Cancha.ts
+++ backend/classes/Cancha.ts
@@ -1,6 +1,10 @@
 import Deportes from "../enums/Deportes"
 import Arrendador from "./Arrendador"
 import Direccion from "../interfaces/Direccion"
+import Seguridad from "./Seguridad";
+import CriteriosCancha from "../interfaces/Cancha";
+import DAODireccion from "../dao/DAODireccion";
+import DAOCancha from "../dao/DAOCancha";
 
 class Cancha {
     protected id_cancha: string
@@ -24,9 +28,27 @@ class Cancha {
         this.direccion = direccion
         this.deportesDisponibles = deportesDisponibles
     }
-    public static crearCancha() {
-
-    }
+    public static async crearCancha(
+        idCancha: string, nombreLocal: string, arrendador: Arrendador, aforo: number, direccion: Direccion, deportesDisponibles: Deportes[]) {
+            const criteriosCancha: CriteriosCancha = {
+                id_sportfield : Seguridad.generarUUID(),
+                id_address: direccion.direccion,
+                name_txt: nombreLocal,
+                capacity: aforo
+            }
+            const criteriosDireccion = {
+                id_address : Seguridad.generarUUID(),
+                address_name : direccion.direccion,
+                zip_code : direccion.codigoPostal,
+                city : direccion.ciudad,
+                county : direccion.provincia,
+                state_name : direccion.departamento,
+                country : direccion.pais
+            }
+            await new DAOCancha().insertar(criteriosCancha)
+            await new DAODireccion().insertar(criteriosDireccion)
+            return new Cancha(idCancha, nombreLocal, arrendador, aforo, direccion, deportesDisponibles);
+        }
     public verInfo() {
         
     }
--- backend/dao/DAOCancha.ts
+++ backend/dao/DAOCancha.ts
@@ -1,9 +1,28 @@
+import Conexion from "../database/Conexion";
+import Consulta from "../database/Consulta";
+import DB from "../database/DB";
+import PgDB from "../database/PgDB";
 import CampoBusqueda from "../enums/CampoBusqueda";
 import DAO from "./DAO";
 
+
 class DAOCancha extends DAO {
-    public insertar() {
-        throw new Error("Method not implemented.");
+    private database: DB = new PgDB
+    private connection: Conexion = this.database.getConexion()
+    private consulta: Consulta = this.database.getConsulta()
+
+    public async insertar(criterios: any) {
+        const query = `INSERT INTO customer VALUES ('${criterios.id_sportfield}', '${criterios.id_address}', '${criterios.name_txt}', '${criterios.capacity}')`
+        try {
+            await this.connection.open()
+            this.consulta.set(query)
+            await this.consulta.execute()
+            await this.connection.close()
+        }
+        catch (error) {
+            console.error(error)
+            await this.connection.close()
+        }
     }
     public seleccionarUno(_criterio: string, _campoBusqueda: CampoBusqueda) {
         throw new Error("Method not implemented.");
