3120866039432b80507dea2e1d3bfc0545188038
JorgeCostilla1 <20190542@aloe.ulima.edu.pe>
2023-07-18 00:09:22 -0500

    js reserva

--- website-frontend/scripts/reserva.js
+++ website-frontend/scripts/reserva.js
@@ -4,23 +4,37 @@ form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   // Obtener los valores ingresados en el formulario
-  const nombreCliente = document.getElementById("nombre").value;
-  const fechaHora = new Date(document.getElementById("fecha").value);
-  const turno = document.getElementById("Horario").value;
-
-  const mensaje = {
-    nombreCliente,
-    fechaHora,
-    turno
-  };
+  const fecha = new Date(document.getElementById("fecha").value);
+  const inicio = document.getElementById("inicio").value;
+  const fin = document.getElementById("fin").value;
+  
+  let idCancha = await fetch('http://localhost:3001/api/cancha/getid', {
+    method: 'GET',
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+    
+let idUser = await fetch('http://localhost:3001/api/usuario/getid', {
+  method: 'GET',
+  headers: {
+      "Content-Type": "application/json"
+  }
+})
 
   try {
-    let response = await fetch("http://localhost:3001/api/reservar_cancha", {
+    let response = await fetch("http://localhost:3001/api/cancha/reserva", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(mensaje)
+      body: JSON.stringify({
+        dateBooking:fecha,
+        timeStart: inicio,
+        timeEnd: fin,
+        sportfireldId: idCancha,
+        userId: idUser
+      })
     });
 
     const data = await response.json();
@@ -30,13 +44,6 @@ form.addEventListener("submit", async (event) => {
       // Mostrar mensaje de éxito
       alert("¡Reserva realizada con éxito!");
 
-      // Obtener datos adicionales de la cancha desde el backend
-      const idCancha = data.idCancha;
-      const canchaResponse = await obtenerCanchaPorId(idCancha);
-
-      // Obtener datos adicionales del arrendador desde el backend
-      const idArrendador = canchaResponse.msg.idArrendador;
-      const arrendadorResponse = await obtenerUsuarioPorId(idArrendador);
 
       if (canchaResponse && arrendadorResponse) {
         const cancha = canchaResponse.msg;
@@ -60,42 +67,3 @@ form.addEventListener("submit", async (event) => {
   }
 });
 
-async function obtenerCanchaPorId(id) {
-  const mensaje = { msg: id };
-
-  try {
-    let response = await fetch("http://localhost:3001/api/buscar_cancha_id", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(mensaje)
-    });
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al obtener la información de la cancha:", error);
-    return null;
-  }
-}
-
-async function obtenerUsuarioPorId(id) {
-  const mensaje = { msg: id };
-
-  try {
-    let response = await fetch("http://localhost:3001/api/buscar_usuario_id", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(mensaje)
-    });
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al obtener la información del arrendador:", error);
-    return null;
-  }
-}
